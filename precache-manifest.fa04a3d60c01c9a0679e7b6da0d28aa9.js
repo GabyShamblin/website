690c5e5892edca1116e214463c1873dfba1352e9
Gabriela <gabyshamblin@gmail.com>
2023-05-18 14:31:23 -0400

    Updates

---
+++ precache-manifest.fa04a3d60c01c9a0679e7b6da0d28aa9.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c9f66e0f3f7ab94f55f",
+    "url": "/static/css/main.c4a3ad49.chunk.css"
+  },
+  {
+    "revision": "9c9f66e0f3f7ab94f55f",
+    "url": "/static/js/main.412dbbb9.chunk.js"
+  },
+  {
+    "revision": "f73a264dd601b9cf107a",
+    "url": "/static/js/runtime~main.e853599e.js"
+  },
+  {
+    "revision": "91ad00f19bbe764d06d3",
+    "url": "/static/js/2.77f7b433.chunk.js"
+  },
+  {
+    "revision": "ca79530209ece39a569e",
+    "url": "/static/js/3.bfa3f112.chunk.js"
+  },
+  {
+    "revision": "40a4d708232924a7e3104718e93faa0d",
+    "url": "/static/media/car.40a4d708.jpg"
+  },
+  {
+    "revision": "364ec09ce3092577e8de89274e724e27",
+    "url": "/static/media/heart.364ec09c.jpg"
+  },
+  {
+    "revision": "e68d55d2179668d7b5b627e4862b4cf4",
+    "url": "/static/media/picture.e68d55d2.jpg"
+  },
+  {
+    "revision": "98e9f8f1fd642a5f5b1dbb192a798778",
+    "url": "/static/media/HeartWin64.98e9f8f1.zip"
+  },
+  {
+    "revision": "2165c701c2e9de11c2bcfa30f16f308b",
+    "url": "/static/media/HeartMac.2165c701.zip"
+  },
+  {
+    "revision": "dd0dd505b88ccf5d5d945c17ef1a025e",
+    "url": "/static/media/grayWebsite.dd0dd505.jpg"
+  },
+  {
+    "revision": "75ef706ab738a6ed06f0a09ff111ef67",
+    "url": "/static/media/pythonMenu.75ef706a.jpg"
+  },
+  {
+    "revision": "062e360b838c0321f70cb007d87cfa04",
+    "url": "/static/media/scheduler.062e360b.jpg"
+  },
+  {
+    "revision": "bf780f20a03af4e0c91624b3f3b93879",
+    "url": "/static/media/Group 1 TOTS Presentation.bf780f20.pdf"
+  },
+  {
+    "revision": "a0c320b6415093d07c05f4765fe93ee0",
+    "url": "/static/media/contactManager.a0c320b6.jpg"
+  },
+  {
+    "revision": "c9a17ca0fb183c92286705e12fc1f069",
+    "url": "/static/media/Small Project.c9a17ca0.pdf"
+  },
+  {
+    "revision": "4b98c57772e345c381357a8cd05e9cf3",
+    "url": "/static/media/diceGame.4b98c577.jpg"
+  },
+  {
+    "revision": "9f272a1f4f5716e237807be2e3f74fc4",
+    "url": "/static/media/Gabriela Shamblin Resume.9f272a1f.pdf"
+  },
+  {
+    "revision": "2fb0a23ae38ff99569c176bd96191629",
+    "url": "/static/media/angular.2fb0a23a.png"
+  },
+  {
+    "revision": "875e243185a83046c4bd0711102e9b86",
+    "url": "/static/media/c.875e2431.png"
+  },
+  {
+    "revision": "22a5816e7136a2b2917013ed592819f8",
+    "url": "/static/media/c++.22a5816e.png"
+  },
+  {
+    "revision": "61a16f074d7797b8729456c45546aff8",
+    "url": "/static/media/csharp.61a16f07.png"
+  },
+  {
+    "revision": "53bac1cdc47b2d8949eab6d98d5527fb",
+    "url": "/static/media/css.53bac1cd.png"
+  },
+  {
+    "revision": "5a017d274a8795e924134db532c88592",
+    "url": "/static/media/haskell.5a017d27.png"
+  },
+  {
+    "revision": "a3161e6e17600f4885e8536b704c145b",
+    "url": "/static/media/html.a3161e6e.png"
+  },
+  {
+    "revision": "444c544b04825ac1582b707032fe3d96",
+    "url": "/static/media/java.444c544b.png"
+  },
+  {
+    "revision": "78a2de75d3d3c19b7c9cec24a1082ab1",
+    "url": "/static/media/javascript.78a2de75.png"
+  },
+  {
+    "revision": "def862824791ffb29d1eb3104f96258d",
+    "url": "/static/media/python.def86282.png"
+  },
+  {
+    "revision": "22808bae77c66bd1b33e298ea2f4320e",
+    "url": "/static/media/react.22808bae.png"
+  },
+  {
+    "revision": "0d27acfc9e0690a5b594a5bfb744a941",
+    "url": "/static/media/typescript.0d27acfc.png"
+  },
+  {
+    "revision": "6cc2a5bea67abfa61069526be6cdffe5",
+    "url": "/static/media/unity.6cc2a5be.png"
+  },
+  {
+    "revision": "6e66e139d595ddd08de2662b594e2b7b",
+    "url": "/index.html"
+  }
+];
